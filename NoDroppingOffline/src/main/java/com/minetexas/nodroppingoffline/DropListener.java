1d7c2b163f9ec1721b29187844f714058710f2e2
Nathan Stoltenberg <ataranlen@gmail.com>
2016-12-19 13:45:49 -0600

    Prevent players from Moving items to or from a chest after it is broken.

--- NoDroppingOffline/src/main/java/com/minetexas/nodroppingoffline/DropListener.java
+++ NoDroppingOffline/src/main/java/com/minetexas/nodroppingoffline/DropListener.java
@@ -4,7 +4,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.Inventory;
 
 import com.minetexas.util.MTLog;
 
@@ -16,6 +19,20 @@ public class DropListener implements Listener {
 			MTLog.debug(player.getDisplayName() +" dropped item while offline!");
 			e.setCancelled(true);
 			
+		}
+	}
+	
+	//Prevent players from Moving items to or from a chest after it is broken.
+	@EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerInventoryClickEvent(InventoryClickEvent e) {
+		Inventory source = e.getInventory();
+		if (source.getType() == InventoryType.ENDER_CHEST) {
+			return;
+		}
+		
+		if (source.getHolder() == null) {
+			MTLog.debug(e.getWhoClicked().getName() +" source null clicked item from invalid inventory.");
+			e.setCancelled(true);
 		}
 	}
 	
